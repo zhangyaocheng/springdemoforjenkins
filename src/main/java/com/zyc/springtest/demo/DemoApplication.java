e10b69773ba57934d967a9d3b3945e1b89aa40a8
zhangyaocheng <910486122@qq.com>
2020-11-13 00:52:43 +0800

    修改 DemoApplication适应远程打包

--- src/main/java/com/zyc/springtest/demo/DemoApplication.java
+++ src/main/java/com/zyc/springtest/demo/DemoApplication.java
@@ -2,12 +2,21 @@ package com.zyc.springtest.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
+    // 按照下面的方式重写
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
+
 
 }
